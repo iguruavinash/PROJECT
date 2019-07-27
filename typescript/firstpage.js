e11cf8ee486f8f77653b6d0ace72a2c710deef31
Sunil Mekala <iguruavinash@gmail.com>
2019-07-27 21:41:46 +0530

    firstpage

---
+++ typescript/firstpage.js
@@ -0,0 +1,12 @@
+var name_1 = "guru";
+console.log(name_1);
+for (var i = 0; i < 10; i = i + 1) {
+    console.log(i);
+}
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add( 3,5);
+// console.log(result);
---
+++ typescript/firstpage.ts
@@ -0,0 +1,33 @@
+// let name_1 = "guru";
+// console.log(name_1);
+for (var i = 0; i < 10; i = i + 1){
+    // console.log
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add( 3,5);
+console.log(result);
+
+let a = [1,2,3,4,5];
+
+for (var i = 0; i < a.length; i++){
+    if(a[i] % 2 == 0) {
+    console.log(a[i]);
+
+    }
+}
+ let s = 'liril';
+
+ function checkPalandrom(inputStr) {
+
+    // logic to find if it is panadrom or not
+
+    return true;
+ }
+
+ // fibinoci series
+
+ 
