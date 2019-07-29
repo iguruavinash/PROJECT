eefd3ff5b95e85e047b04432bc1d84f36d904839
Sunil Mekala <iguruavinash@gmail.com>
2019-07-29 20:25:28 +0530

    constant declaration

---
+++ typescript/constantdeclaration.js
@@ -0,0 +1,12 @@
+let num1;
+const num = 2;
+const obj1 = {
+    name: "guru"
+};
+console.log(obj1.name);
+obj1.name = "avinash";
+console.log(obj1.name);
+let input = [1, 2];
+let [first, second] = input;
+console.log(first); // outputs 1
+console.log(second); // outputs 2
