30372bf971331f662b9e1b8f82583d17ead316fe
Sunil Mekala <iguruavinash@gmail.com>
2019-08-06 13:14:35 +0530

    practice

---
+++ typescript/lamda.ts
@@ -0,0 +1,59 @@
+console.log("testNum", testNum);
+// console.log(arr1);
+// console.log(name_1);
+// console.log(test_1);
+var testNum = 10;
+let name_1 = "ravi";
+console.log(name_1);
+// function printOut(i){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000)
+// }
+// for(var i = 0 ; i < 10 ; i = i + 1 ){
+//    printOut(i);
+// }
+// function printOut(i){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000)
+// }
+for (let i = 0; i < 10; i = i + 1) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+;
+var divEl = document.getElementsByTagName('div')[0];
+var data = ['ravi', 'krishna'];
+divEl.onclick = run(data);
+function run(data) {
+    return function (e) {
+        console.log("name:", data[0], e);
+    };
+}
+const num = 10;
+// num = 10;
+console.log(num);
+let obj = {
+    name: "krishna"
+};
+obj.name = "radhekrishna";
+// obj.name = 10;
+const arr = [10, 20, 30, ""];
+arr.push(40);
+arr.push("ravi");
+arr.unshift(0);
+let poppedItem = arr.pop();
+let shiftedItem = arr.shift();
+const arr1 = [15, 25, 35, "", 45];
+arr1.splice(3, 1, 40);
+var arr2 = arr1.slice(1, 3);
+let test_1 = () => {
+    console.log('arrow function');
+    console.log(this);
+    return function () {
+        console.log("test");
+    };
+};
+test_1();
