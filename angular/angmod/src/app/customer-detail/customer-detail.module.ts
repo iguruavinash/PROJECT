be8da0148bee193a687709a8893671a8a531561b
Sunil Mekala <iguruavinash@gmail.com>
2019-09-09 12:21:03 +0530

    customersmodule'

---
+++ angular/angmod/src/app/customer-detail/customer-detail.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerDetailComponent } from './customer-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CustomerDetailComponent],
+  exports: [CustomerDetailComponent]
+})
+export class CustomerDetailModule { }
