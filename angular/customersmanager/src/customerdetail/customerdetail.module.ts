f7645cc371cac6f6ae15418a076bfd386dc2c37e
Sunil Mekala <iguruavinash@gmail.com>
2019-09-14 20:50:41 +0530

    customersmanager

---
+++ angular/customersmanager/src/customerdetail/customerdetail.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerdetailRoutingModule } from './customerdetail-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomerdetailRoutingModule
+  ]
+})
+export class CustomerdetailModule { }
