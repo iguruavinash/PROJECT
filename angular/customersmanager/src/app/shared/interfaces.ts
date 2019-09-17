de6a678a04ba8b67efdaa23314c3ab261bc3e215
Sunil Mekala <iguruavinash@gmail.com>
2019-09-17 23:33:07 +0530

    services

--- angular/customersmanager/src/app/shared/interfaces.ts
+++ angular/customersmanager/src/app/shared/interfaces.ts
@@ -1,24 +1,24 @@
 export interface ICustomer {
-    'id': Number;
-    'firstName': String;
-    'lastName':String;
-    'gender': String;
-    'address': String;
-    'city': String;
+    'id': 'Number';
+    'firstName': 'String';
+    'lastName': 'String';
+    'gender': 'String';
+    'address': 'String';
+    'city': 'String';
     'state': {
-        'abbreviation': String;
-        'name': String
+        'abbreviation': 'String';
+        'name': 'String'
     };
     'orders': [
         {
-            'productName': String;
-            'itemCost': Number
+            'productName': 'String';
+            'itemCost': 'Number'
         },
         {
-            'productName': String;
-            'itemCost': Number
+            'productName': 'String';
+            'itemCost': 'Number'
         }
     ];
-    'latitude'?: Number;
-    'longitude'?: Number;
+    'latitude'?: 'Number';
+    'longitude'?: 'Number';
 }
